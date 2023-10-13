f8fa81f7ab4a5f6676473d5c6ac6a33b0e322a12
Raelinda Shoates <redfire809@gmail.com>
2023-10-13 15:27:04 -0400

    added a function that counts the different flavors and organizes the order

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 //vanilla,vanilla,vanilla,strawberry,coffee,coffee
 
-let input = prompt('What froyo flavors would you like?');
+let input = window.prompt('What froyo flavors would you like?');
+let flavors = input.split(",");
+let order = {};
+
+function flavorCount (list) {
+        let vanilla = 0;
+        let strawberry = 0;
+        let coffee = 0;
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === 'vanilla') {
+               vanilla ++;
+               order.vanilla = vanilla;
+            }
+            if (list[i] === 'strawberry') {
+                strawberry ++;
+                order.strawberry = strawberry;
+             }
+             if (list[i] === 'coffee') {
+                coffee ++;
+                order.coffee = coffee;
+             }
+        }
+           return order;
+    }
+
+    console.log(flavorCount(flavors));
+
+
+
+    
+
+
+
+
+
 
